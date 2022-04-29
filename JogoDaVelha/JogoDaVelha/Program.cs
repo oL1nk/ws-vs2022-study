244f269e555ec79e98303c3c151889bec3da99e4
Lucas Almeida <adventureshiny@gmail.com>
2022-04-29 06:30:17 -0300

    deus agora vai

--- JogoDaVelha/JogoDaVelha/Program.cs
+++ JogoDaVelha/JogoDaVelha/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace JogoDaVelha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new JogoDaVelha().Iniciar();
+
+        }
+    }
+}
